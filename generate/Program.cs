d1a948d2cc818dcef074df7c795e170fe7f72955
b-abv <b-abv@microsoft.com>
2017-04-23 16:28:36 +0530

    removed bold

--- generate/Program.cs
+++ generate/Program.cs
@@ -15,13 +15,15 @@ namespace generate
             
             foreach(var file in Directory.EnumerateFiles("./CVs"))
             {
-                var parts = file.Split('\\');
+                File.Move(file, file.Replace(" ","-"));
+                var new_file = file.Replace(" ","-");
+                var parts = new_file.Split('\\');
                 Console.WriteLine("Filename : " + file + " parts : " + parts[0]);
                 string outputPath = "./Output/" + parts[1].Split('.')[0] + ".json";
                 Console.WriteLine("Output path is : " + outputPath);
-                string fullPath = Path.GetFullPath(file);
+                string fullPath = Path.GetFullPath(new_file);
                 ProcessStartInfo info;
-                if(Path.GetExtension(file) == ".docx")
+                if(Path.GetExtension(new_file) == ".docx")
                 {
                     info  = new ProcessStartInfo("python", "../docx2json.py " + fullPath + " " + outputPath);
                     Process python = new Process();
@@ -30,14 +32,14 @@ namespace generate
                     python.WaitForExit();
                 }
 
-                else if(Path.GetExtension(file) == ".pdf")
+                else if(Path.GetExtension(new_file) == ".pdf")
                 {
                     info = new ProcessStartInfo("python", "../pdf2txt.py -t xml -o output.xml " + fullPath);
                     Process python = new Process();
                     python.StartInfo = info;
                     python.Start();
                     python.WaitForExit();
-                    info = new ProcessStartInfo("python", "../xmlParser.py " + outputPath);
+                    info = new ProcessStartInfo("python", "../xmlParser.py " + outputPath + " " + fullPath);
                     python = new Process();
                     python.StartInfo = info;
                     python.Start();
